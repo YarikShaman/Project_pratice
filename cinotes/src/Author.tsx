ad704c145d7cee3e9190c4e9a194edd6def699d2
EquesAuratus <illia.vysochyn@nure.ua>
2023-04-19 14:44:03 +0300

    comit

---
+++ cinotes/src/Author.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.png';
+import './Author.css';
+
+function Author() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a className="App-logo">
+                    Cinotes
+                </a>
+                {/*<p>*/}
+                {/*  Edit <code>src/App.tsx</code> and save to reload.*/}
+                {/*</p>*/}
+                {/*<a*/}
+                {/*  className="App-link"*/}
+                {/*  href="https://reactjs.org"*/}
+                {/*  target="_blank"*/}
+                {/*  rel="noopener noreferrer"*/}
+                {/*>*/}
+                {/*  Learn React*/}
+                {/*</a>*/}
+            </header>
+            <body className="App-body">
+            Lalala1
+            </body>
+            <footer className="App-footer">
+
+            </footer>
+        </div>
+    );
+}
+
+export default Author;
