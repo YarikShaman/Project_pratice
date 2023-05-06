3739ccdf57d5749f06b8e06d1bc53c9c7b36c087
EquesAuratus <illia.vysochyn@nure.ua>
2023-05-06 16:20:07 +0300

    search

--- cinotes/src/Pages/Films.tsx
+++ cinotes/src/Pages/Films.tsx
@@ -4,14 +4,12 @@ import {HomeHeader} from "../Components/HomeHeader";
 
 export function Films() {
     return (
-        <div className="App">
+        <div className="h-screen flex flex-col">
             <HomeHeader/>
-            <body className="App-body">
-            Films
-            </body>
-            <footer className="App-footer">
-
-            </footer>
+            <div className="w-4/6 mt-5 flex flex-nowrap self-center h-10">
+                <input className=" flex-1 bg-slate-800 ring-slate-700 text-white block  rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type={"text"}/>
+                <button className=" flex-5 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type={"submit"}>Search</button>
+            </div>
         </div>
     );
 }
