5d59019a3d42e03f5a83fcdd671aa1ccdc41de0c
EquesAuratus <illia.vysochyn@nure.ua>
2023-05-07 18:17:23 +0300

    Header

--- cinotes/src/Components/HomeHeader.tsx
+++ cinotes/src/Components/HomeHeader.tsx
@@ -1,39 +1,41 @@
 import React from "react";
 import "../App.css";
 import Acc from '../Img/Account.png';
-
+import {Link} from "react-router-dom";
 export function HomeHeader() {
     return (
-        <span className="bg-neutral-800  flex flex-row align-center content-center" >
+        <div className="bg-neutral-800 flex flex-row space-x-5 w-full items-center justify-between" >
             <a className="self-center pl-6 text-2xl font-bold text-white select-none italic ">
                 Cinotes
             </a>
-            <span className=" self-center flex justify-evenly w-full ">
-                <div className="bg-sky-900  h-8 w-16 rounded-lg flex justify-center text-white hover:text-sky-700  cursor-pointer">
-                    <a href={"/home"} className="self-center text-base  align-middle ">
-                        Home
-                    </a>
+            <div className={"flex flex-row w-full items-center justify-between"}>
+                <div className="flex w-1/4">
+                    <Link className={"flex flex-grow"} to={"/"}>
+                        <div className="hover:bg-sky-900 h-20 rounded-lg py-7 flex self-center flex-grow justify-center text-white  cursor-pointer">
+                            Home
+                        </div>
+                    </Link>
+                    <Link className={"flex flex-grow"} to={"/films"}>
+                        <div className="hover:bg-sky-900 rounded-lg h-20 flex py-7 self-center flex-grow justify-center text-white cursor-pointer">
+                            Films
+                        </div>
+                    </Link>
+                    <Link className={"flex flex-grow"} to={"/playlists"}>
+                        <div className="hover:bg-sky-900 rounded-lg h-20 flex py-7 self-center flex-grow justify-center text-white cursor-pointer">
+                            PlayLists
+                        </div>
+                    </Link>
+                    <Link className={"flex flex-grow"} to={"/actors"}>
+                        <div className="hover:bg-sky-900 rounded-lg h-20 flex py-7 self-center flex-grow justify-center text-white cursor-pointer">
+                            Actors
+                        </div>
+                    </Link>
                 </div>
-                <div className="bg-sky-900  h-8 w-16 rounded-lg flex justify-center text-white hover:text-sky-700  cursor-pointer">
-                    <a href={"/films"} className="self-center text-base text-white align-middle cursor-pointer hover:text-sky-700">
-                        Films
-                    </a>
+                <div className="hover:bg-neutral-900 bg-stone-900 my-1 flex flex-col w-20 border-2 border-gray-800 rounded-xl mr-2 text-white hover:text-sky-500 hover:border-sky-900 cursor-pointer select-none">
+                    <img className=" w-7 h-7 mt-1 bg-cover bg-repeat self-center " alt={"Profile_Pic"} src={Acc}></img>
+                    <a href={"/sign_in"} className="text-sm self-center text-center">Log in</a>
                 </div>
-                <div className="bg-sky-900  h-8 w-16 rounded-lg flex justify-center text-white hover:text-sky-700  cursor-pointer">
-                    <a href={"/playlists"} className="self-center text-base text-white align-middle cursor-pointer hover:text-sky-700">
-                        PlayLists
-                    </a>
-                </div>
-                <div className="bg-sky-900  h-8 w-16 rounded-lg flex justify-center text-white hover:text-sky-700  cursor-pointer">
-                    <a href={"/actors"} className=" bg-blend-normal  self-center text-base text-white align-middle cursor-pointer hover:text-sky-700">
-                        Actors
-                    </a>
-                </div>
-            </span>
-            <span className="justify-center hover:bg-neutral-900 bg-stone-900 my-1 flex flex-col w-20 border-2 border-gray-800 rounded-xl mr-2 text-white hover:text-sky-500 hover:border-sky-900 cursor-pointer select-none">
-                <img className=" w-7 h-7 mt-1 bg-cover bg-repeat self-center " alt={"Profile_Pic"} src={Acc}></img>
-                <a href={"/sign_in"} className="text-sm self-center text-center">Log in</a>
-            </span>
-        </span>
+            </div>
+        </div>
     );
 }
