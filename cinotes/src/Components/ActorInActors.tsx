30aa6501d2595290e9a4e87c2f5a36cd7b5822bd
EquesAuratus <illia.vysochyn@nure.ua>
2023-05-21 16:10:53 +0300

    Actors

--- cinotes/src/Components/ActorInActors.tsx
+++ cinotes/src/Components/ActorInActors.tsx
@@ -5,7 +5,7 @@ export function ActorInActors(actor:any){
     return(
         <>
             <div className={"hover:bg-gray-900 flex shadow-gray-900 flex-col bg-neutral-800 rounded-xl border-2 border-stone-900 shadow-lg w-64 h-96 hover:shadow-white mx-5 my-5"}>
-                <img className="w-5/6 mt-4 h-5/6 w-auto rounded-xl border-2 border-gray-700 self-center" src={actor.actor.photo_file}/>
+                <img className="w-60 mt-4 h-5/6 w-auto rounded-xl object-cover border-2 border-gray-700 self-center" src={actor.actor.photo_file}/>
                 <a className="text-white self-center my-auto text-center w-5/6">{actor.actor.name}</a>
             </div>
         </>
