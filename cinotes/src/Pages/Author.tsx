a634e4edca03b72fb27755f88915b18d12db960f
EquesAuratus <illia.vysochyn@nure.ua>
2023-05-30 12:02:18 +0300

    show/hide password buttons

--- cinotes/src/Pages/Author.tsx
+++ cinotes/src/Pages/Author.tsx
@@ -14,6 +14,9 @@ import {useNavigate} from "react-router-dom";
 function Author() {
     const [error, setError] = useState(String);
     const [navigate, setNavigate] = useState(false);
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
     const nav = useNavigate()
 
     function Signin(login: string, password: string) {
@@ -105,8 +108,6 @@ function Author() {
         }
     });
 
-    const [login, setLogin] = useState("");
-    const [password, setPassword] = useState("");
 
 
     return (
@@ -144,7 +145,7 @@ function Author() {
                                        type="text"
                                        autoComplete="login"
                                        required
-                                       className="bg-slate-800 ring-slate-700 text-white block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                       className=" ring-slate-700 text-white block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -162,18 +163,31 @@ function Author() {
                                     </a>
                                 </div>
                             </div>
-                            <div className="mt-2">
+                            <div className="mt-2 relative">
                                 <input onChange={(e) => {
                                     setPassword(e.target.value)
                                 }}
                                        id="password"
                                        name="password"
-                                       type="password"
+                                       type={showPassword ? 'text' : 'password'}
                                        autoComplete="current-password"
                                        required
-                                       className="bg-slate-800 ring-slate-700 text-white block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                       className="ring-slate-700 text-white block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
+                                <button className={"text-white  m-[1px] absolute right-0 top-0 flex items-center justify-center rounded-xl h-[34px] w-[34px] "} onClick={(e)=>{e.preventDefault(); setShowPassword(!showPassword)}}>
+                                    {showPassword ?<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className=" w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        </svg>
+                                        : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                               strokeWidth="1.5" stroke="black" className=" w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"/>
+                                        </svg>
+                                    }
+                                </button>
                             </div>
+
                             <div className={"text-red-700"}>{error}</div>
                         </div>
 
--- cinotes/src/Pages/Registr.tsx
+++ cinotes/src/Pages/Registr.tsx
@@ -14,6 +14,8 @@ function Registr() {
     const [email, setEmail] = useState("");
     const [pas1, setPas1] = useState("");
     const [pas2, setPas2] = useState("");
+    const [showPassword1, setShowPassword1] = useState(false);
+    const [showPassword2, setShowPassword2] = useState(false);
     const nav = useNavigate()
     const [errorUser, setErrorUser] = useState("")
     const [errorEmail, setErrorEmail] = useState("")
@@ -105,7 +107,7 @@ function Registr() {
                                     type="text"
                                     autoComplete="username"
                                     required
-                                    className="bg-slate-800 ring-slate-700 text-white block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="ring-slate-700 block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                             <div className={"text-red-700"}>{errorUser}</div>
@@ -124,7 +126,7 @@ function Registr() {
                                        type="email"
                                        autoComplete="email"
                                        required
-                                       className="bg-slate-800 ring-slate-700 text-white block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                       className="ring-slate-700 block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                             <div className={"text-red-700"}>{errorEmail}</div>
@@ -134,7 +136,7 @@ function Registr() {
                             <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
                                 {GetLang().Password}
                             </label>
-                            <div className="mt-2">
+                            <div className="mt-2 relative">
                                 <input onChange={(e) => {
                                     setPas1(e.target.value)
                                     if (CheckPas(pas1).code == 0)
@@ -142,11 +144,23 @@ function Registr() {
                                 }}
                                        id="password"
                                        name="password"
-                                       type="password"
+                                       type={showPassword1 ? 'text' : 'password'}
                                        autoComplete="current-password"
                                        required
-                                       className="bg-slate-800 ring-slate-700 text-white block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                       className=" ring-slate-700 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
+                                <button className={"text-white  m-[1px] absolute right-0 top-0 flex items-center justify-center rounded-xl h-[34px] w-[34px] "} onClick={(e)=>{e.preventDefault(); setShowPassword1(!showPassword1)}}>
+                                    {showPassword1 ?<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className=" w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        </svg>
+                                        : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                               strokeWidth="1.5" stroke="black" className=" w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"/>
+                                        </svg>
+                                    }
+                                </button>
                             </div>
                             <div className={"text-red-700"}>{error1}</div>
                         </div>
@@ -155,7 +169,7 @@ function Registr() {
                                    className="block text-sm font-medium leading-6 text-white">
                                 {GetLang().Repeat_password}
                             </label>
-                            <div className="mt-2">
+                            <div className="mt-2 relative">
                                 <input onChange={(e) => {
                                     setPas2(e.target.value)
                                     if (pas1 == pas2)
@@ -163,11 +177,23 @@ function Registr() {
                                 }}
                                        id="password-repeat"
                                        name="password-repeat"
-                                       type="password"
+                                       type={showPassword2 ? 'text' : 'password'}
                                        autoComplete="current-password"
                                        required
-                                       className="bg-slate-800 ring-slate-700 text-white block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                       className="ring-slate-700 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
+                                <button className={"text-white  m-[1px] absolute right-0 top-0 flex items-center justify-center rounded-xl h-[34px] w-[34px] "} onClick={(e)=>{e.preventDefault(); setShowPassword2(!showPassword2)}}>
+                                    {showPassword2 ?<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className=" w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        </svg>
+                                        : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                               strokeWidth="1.5" stroke="black" className=" w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"/>
+                                        </svg>
+                                    }
+                                </button>
                             </div>
                             <div className={"text-red-700"}>{error2}</div>
                         </div>
