f10e63cfc9c57869cdd407822d7be5a513465b1f
EquesAuratus <illia.vysochyn@nure.ua>
2023-05-31 09:25:08 +0300

    several fixes

--- cinotes/src/Components/HomeHeader.tsx
+++ cinotes/src/Components/HomeHeader.tsx
@@ -163,12 +163,12 @@ export function HomeHeader() {
                         </div>
                         <Link to={link}>
                             <div
-                                className="hover:bg-neutral-900 bg-neutral-800 my-1 flex flex-col w-20 border-2 border-gray-700 rounded-xl mr-2 text-white hover:text-sky-500 hover:border-sky-900 cursor-pointer select-none">
+                                className="hover:bg-neutral-900 bg-neutral-800 my-1 flex flex-col w-[100px] border-2 border-gray-700 rounded-xl mr-2 text-white hover:text-sky-500 hover:border-sky-900 cursor-pointer select-none">
                                 <img className=" w-7 h-7 mt-1 bg-cover bg-repeat rounded-full self-center "
                                      id={"ProfileImg"}
                                      alt={"Profile_Pic"}
                                      src={source}></img>
-                                <p className="text-sm self-center text-center">{label}</p>
+                                <p className="text-sm self-center text-wrap text-center">{label}</p>
                             </div>
                         </Link>
 
